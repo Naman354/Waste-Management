f741c8dfa3b0803af069578426a824df680cc8f2
Naman354 <srinaman05@gmail.com>
2026-03-26 23:33:20 +0530

    Added frontend to cors

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ connectDB();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173'],
+  origin: ['http://localhost:8080', 'https://wastemanagementmc.netlify.app'],
   credentials: true
 }));
 app.use(express.json());
