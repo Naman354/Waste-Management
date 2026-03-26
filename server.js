95375eb50f11ed707876b618a7d872bf4fe59033
Naman354 <srinaman05@gmail.com>
2026-03-26 23:00:44 +0530

    Added cors

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db.js');
 
@@ -10,6 +11,10 @@ const app = express();
 connectDB();
 
 // Middleware
+app.use(cors({
+  origin: ['http://localhost:5173'],
+  credentials: true
+}));
 app.use(express.json());
 
 // Routes
