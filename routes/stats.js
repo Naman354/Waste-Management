2d3b1fdebeed1400e81728b873689cfbe58f7d8d
Shivansh6317 <shivanxhskumar@gmail.com>
2026-03-27 23:28:02 +0530

    graph api

---
+++ routes/stats.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const { getStats, getTrends, getCategoryStats, getWardStats } = require('../controllers/statsController.js');
+
+// GET /stats
+router.get('/', getStats);
+
+// GET /stats/trends?range=24h|7d|30d
+router.get('/trends', getTrends);
+
+// GET /stats/categories
+router.get('/categories', getCategoryStats);
+
+// GET /stats/wards
+router.get('/wards', getWardStats);
+
+module.exports = router;
